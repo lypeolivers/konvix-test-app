9f04e4f1e35f8b8d15b1c0225e19f8947e04b8c6
Leonardo Gindri <30549746+lypeolivers@users.noreply.github.com>
2024-02-14 17:15:49 -0500

    fix: env api

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: process.env.URL_API + "/",
-  baseURL: "http://localhost:3001/",
+  baseURL: process.env.URL_API + "/",
+
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
